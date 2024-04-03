c15c98ccf9001f1dab538e979c44f164e8859abf
kunal adhav <kunal1603.adhav@gmail.com>
2024-04-03 17:10:36 +0530

    session 3 changes

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 const heading = React.createElement(
   "h1",
@@ -14,7 +14,7 @@ const heading2 = React.createElement(
   "h1",
   {
     id: "title",
-    key: "h1",
+    key: "h2",
   },
   "Heading 2"
 );
