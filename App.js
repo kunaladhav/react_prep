4bb04833553a804fd1114c8fcb60a48149fdd0ec
kunal adhav <kunal1603.adhav@gmail.com>
2024-04-05 17:23:57 +0530

    Session 4 Finished

--- App.js
+++ App.js
@@ -1,35 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h1",
-  },
-  "Heading 1 for parcel"
+const title = (
+  <h1 id="title" key="h1">
+    Namaste Kunal
+  </h1>
 );
 
-const heading2 = React.createElement(
-  "h1",
-  {
-    id: "title",
-    key: "h2",
-  },
-  "Heading 2"
+const Title2 = () => (
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>
 );
+// React Components
 
-const container = React.createElement(
-  "div",
-  {
-    id: "container",
-    hello: "world",
-  },
-  [heading, heading2]
-);
-
-console.log(heading);
+const HeaderComponent = () => {
+  return (
+    <div>
+      {title} /* React element */
+      {Title2()} // React Component
+      <Title2 />
+      <title />
+      <h1>Namaste React Functional Component</h1>
+      <h2>This is a H2 tag</h2>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(container);
+root.render(<HeaderComponent />);
